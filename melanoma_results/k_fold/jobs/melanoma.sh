cff82027c4fc4e96a4a906ab7bb8fa9835291c9e
Haoran Zhang <frankfcba@gmail.com>
2022-05-20 16:21:24 -0500

    clean up

--- melanoma_results/k_fold/jobs/melanoma.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#BSUB -W 96:00
-#BSUB -R rusage[mem=24]
-#BSUB -J melanoma[1-330]
-#BSUB -e ../setup/outputs/melanoma_%I.err
-#BSUB -eo ../setup/outputs/melanoma_%I.out
-
-cd $LS_SUBCWD
-python grid_search_cfg.py --config ../setup/config/melanoma/config_${LSB_JOBINDEX}.cfg
